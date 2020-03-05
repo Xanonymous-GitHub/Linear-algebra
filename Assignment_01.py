2434de40b4caaab9d469907955c4e14f6144bc27
Xanonymous <trusaidlin@gmail.com>
2020-03-05 15:05:58 +0800

    oop

--- Assignment_01.py
+++ Assignment_01.py
@@ -1,25 +1,32 @@
 class Matrix:
+
+    def __init__(self, user_input):
+        self.__storage = user_input
+
+    def __str__(self):
+        return str(self.__storage)
+
     # Matrix addition or subtraction.
-    def combine(self, resource: list, result: list, t=True):
+    def combine(self, result: list, t=True):
         # If the matrices cannot be added or subtracted, throw an error.
-        assert self.has_same_col_length(resource)
-        assert self.has_same_col_length(result)
-        assert (len(resource) == len(result) and len(resource[0]) == len(result[0]))
-        for i, x in enumerate(resource):
+        assert self.__has_same_col_length(self.__storage)
+        assert self.__has_same_col_length(result)
+        assert (len(self.__storage) == len(result) and len(self.__storage[0]) == len(result[0]))
+        for i, x in enumerate(self.__storage):
             for j, y in enumerate(x):
                 result[i][j] += y if t else -1 * y
         return result
 
     # Matrix multiplication
-    def multiply(self, resource, father_result: list):
+    def multiply(self, father_result: list):
         # If the matrices cannot be multiplied, throw an error.
-        assert self.has_same_col_length(father_result)
-        assert (len(g) == len(father_result) for g in resource)
+        assert self.__has_same_col_length(father_result)
+        assert (len(g) == len(father_result) for g in self.__storage)
         # if the second matrix is an identity_matrix, the answer will be the same as first matrix.
         if self.is_unit_matrix(father_result):
-            return resource
+            return self.__storage
         ans = list()
-        for i, x in enumerate(resource):
+        for i, x in enumerate(self.__storage):
             tmp_slice = list()
             for n in self.transpose(father_result):
                 tmp_num = list()
@@ -29,24 +36,25 @@ class Matrix:
             ans.append(tmp_slice)
         return ans
 
-    def inverse(self, r):
-        assert len(r) == 2  # not finished yet, it can only calculate two-by-two.
-        assert self.has_same_col_length(r)
-        rate = 1 / self.determinant(r)
-        r[0][0], r[1][1] = r[1][1], r[0][0]
-        r[0][1] *= -1
-        r[1][0] *= -1
-        return self.magnification_iteration(r, rate)
+    @property
+    def inverse(self):
+        assert len(self.__storage) == 2
+        assert self.__has_same_col_length(self.__storage)
+        self.__storage[0][0], self.__storage[1][1] = self.__storage[1][1], self.__storage[0][0]
+        self.__storage[0][1] *= -1
+        self.__storage[1][0] *= -1
+        return self.__magnification_iteration(self.__storage, 1 / self.__determinant(self.__storage))
 
-    def determinant(self, r):
-        assert self.has_same_col_length(r)
+    def __determinant(self, r):
+        assert self.__has_same_col_length(r)
         assert len(r) == len(r[0])
         if len(r) == 2:
             return r[0][0] * r[1][1] - r[0][1] * r[1][0]
         result = list()
         for i, x in enumerate(r[0]):
             h = (-1 if i % 2 else 1)
-            result.append(self.determinant(self.magnification_iteration(self.get_ans_range(0, i, r), h * x)))
+            result.append(
+                self.__determinant(self.__magnification_iteration(self.__get_ans_range(0, i, r), h * x)))
         return sum(result)
 
     @staticmethod
@@ -61,14 +69,14 @@ class Matrix:
         return True
 
     @staticmethod
-    def has_same_col_length(r):
+    def __has_same_col_length(r):
         for p in range(len(r) - 1):
             if len(r[p]) != len(r[p + 1]):
                 return False
         return True
 
     @staticmethod
-    def get_ans_range(ii, jj, resource: list):
+    def __get_ans_range(ii, jj, resource: list):
         ans = list()
         for i, x in enumerate(resource):
             tmp_slice = list()
@@ -80,7 +88,7 @@ class Matrix:
         return ans
 
     @staticmethod
-    def magnification_iteration(r: list, rate):
+    def __magnification_iteration(r: list, rate):
         for i, x in enumerate(r):
             r[i][0] *= rate
         return r
@@ -91,8 +99,8 @@ def main():
     b = [[5, 6], [7, 8]]
     c = [[0, 2], [1, 0], [-1, 1]]
     # print(multiply(c, a))
-    matrix = Matrix()
-    print(inverse(b))
+    matrix = Matrix(b)
+    print(matrix.inverse)
 
 
 if __name__ == "__main__":
