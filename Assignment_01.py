3da31b60a0a1b62d4b0d72c2e1e1a5d47b0ad7be
Xanonymous <trusaidlin@gmail.com>
2020-03-06 12:08:46 +0800

    ooop

--- Assignment_01.py
+++ Assignment_01.py
@@ -1,27 +1,36 @@
 class Matrix:
 
     def __init__(self, user_input):
+        assert self.__has_same_col_length(user_input), "invalid matrix."
         self.__storage = user_input
 
     def __str__(self):
-        return str(self.__storage)
+        self.pretty(self.__storage)
+
+    @staticmethod
+    def pretty(r):
+        return '\n'.join(map(str, r))
+
+    @property
+    def val(self):
+        return self.__storage
 
     # Matrix addition or subtraction.
-    def combine(self, result: list, t=True):
+    def combine(self, result, t=True):
         # If the matrices cannot be added or subtracted, throw an error.
-        assert self.__has_same_col_length(self.__storage)
-        assert self.__has_same_col_length(result)
-        assert (len(self.__storage) == len(result) and len(self.__storage[0]) == len(result[0]))
+        assert (self.__has_same_col_length(self.__storage) and self.__has_same_col_length(result) and (
+                len(self.__storage) == len(result) and len(self.__storage[0]) == len(result[0]))), "can't combine."
         for i, x in enumerate(self.__storage):
             for j, y in enumerate(x):
                 result[i][j] += y if t else -1 * y
-        return result
+        return self.pretty(result)
 
     # Matrix multiplication
-    def multiply(self, father_result: list):
+    def multiply(self, father_result):
         # If the matrices cannot be multiplied, throw an error.
-        assert self.__has_same_col_length(father_result)
-        assert (len(g) == len(father_result) for g in self.__storage)
+        assert self.__has_same_col_length(father_result), "can not multiply."
+        for g in self.__storage:
+            assert len(g) == len(father_result), "can't not multiply."
         # if the second matrix is an identity_matrix, the answer will be the same as first matrix.
         if self.is_unit_matrix(father_result):
             return self.__storage
@@ -34,16 +43,18 @@ class Matrix:
                     tmp_num.append(y * n[j])
                 tmp_slice.append(sum(tmp_num))
             ans.append(tmp_slice)
-        return ans
+        return self.pretty(ans)
 
     @property
     def inverse(self):
         assert len(self.__storage) == 2
         assert self.__has_same_col_length(self.__storage)
+        determinant = self.__determinant(self.__storage)
+        assert determinant, "can not inverse."
         self.__storage[0][0], self.__storage[1][1] = self.__storage[1][1], self.__storage[0][0]
         self.__storage[0][1] *= -1
         self.__storage[1][0] *= -1
-        return self.__magnification_iteration(self.__storage, 1 / self.__determinant(self.__storage))
+        return self.pretty(self.__magnification_iteration(self.__storage, 1 / determinant))
 
     def __determinant(self, r):
         assert self.__has_same_col_length(r)
@@ -56,6 +67,11 @@ class Matrix:
             result.append(
                 self.__determinant(self.__magnification_iteration(self.__get_ans_range(0, i, r), h * x)))
         return sum(result)
+
+    @staticmethod
+    def __error_handler(msg):
+        print(msg)
+        return msg
 
     @staticmethod
     def transpose(resource: list):
@@ -99,8 +115,9 @@ def main():
     b = [[5, 6], [7, 8]]
     c = [[0, 2], [1, 0], [-1, 1]]
     # print(multiply(c, a))
-    matrix = Matrix(b)
-    print(matrix.multiply(a))
+    matrixb = Matrix(b)
+    matrixa = Matrix(c)
+    print(matrixa.multiply(matrixb.val))
 
 
 if __name__ == "__main__":
