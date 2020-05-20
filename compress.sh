e929959fc447632b7f7557746bfe78d369b9d187
Xanonymous <trusaidlin@gmail.com>
2020-05-21 02:00:36 +0800

    change shellscript

--- compress.sh
+++ compress.sh
@@ -1,3 +1,7 @@
 args=("$@")
+mkdir hw_0${args[0]}
+cp ./*.m ./hw_0${args[0]}
+cp ./*.docx ./hw_0${args[0]}
 zip -r ./108820003_林天佑.zip ./hw_0${args[0]}/*
 mv ./108820003_林天佑.zip ~/Desktop/
+rm -rf ./hw_0${args[0]}
